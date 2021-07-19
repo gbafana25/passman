93491934e01f34725ea6119a9f4a14c3646242ac
gbafana25 <garethkm25@gmail.com>
2021-07-18 19:28:38 -0500

    updated readme and started cli arg parser

--- README.md
+++ README.md
@@ -15,4 +15,29 @@ Password manager for Linux, written in Python 3.  Utilizes gpg and doesn't requi
  - `python3 -m venv env`
 
 ## Usage
- :[
+
+### Configuration
+
+Create a config file in the base directory that you want to use, and name it `config.json`.  Categories must be placed into the config file ahead of time.
+
+```json
+{
+	"username": "user",
+	"email": "foo@bar.com",
+	"store_path": "/path/to/dir",
+	"categories": ["Email", "Code"]
+
+
+}
+```
+
+### Running
+
+`python3 passman.py` or `chmod +x passman.py` and `./passman.py`
+An shell alias can also be added
+
+## TODO
+
+- [ ] Make running from command line easier by adding flags and passing path to conf file
+- [x] ~Organize passwords into files by their corresponding website, and folders of general categories~
+- [x] ~Encrypt and decrypt passwords with `gpg` (via os.system)~
--- passman.py
+++ passman.py
@@ -6,6 +6,8 @@ import sys
 
 
 
+
+
 def read_config_file():
 	with open("config.json", "r") as conf_file:
 		try:
@@ -18,20 +20,40 @@ def read_config_file():
 def create_folders(folders):
 	for path in folders:
 		try:
-			os.mkdir(path, 0o600)
+			os.mkdir(path, 0o700)
 		except FileExistsError:
 			print("\"" + path + "\" folder already exists")
 
 
-def create_pass(email, categories):
-	site_name = input("Name of website: ")
-	password = input("Website Password: ")
+
+
+config_data = read_config_file()
+create_folders(config_data['categories'])
+
+
+def main():
+	if len(sys.argv) < 2:
+		sys.exit("Not enough arguments supplied")
+	if sys.argv[1] == '-c':
+		create_pass(config_data['email'], config_data['categories'])
+		sys.exit()
+	if sys.argv[1] == '-u':
+		get_pass(config_data['categories'])		
+
+
+def check_category(categories):
 	for c in categories:
 		print(c)
 	cat = input("Category (case sensitive): ")
 	if cat not in categories:
 		sys.exit("Category was not spelled correctly")
 	os.chdir(cat)
+
+
+def create_pass(email, categories):
+	site_name = input("Name of website: ")
+	password = input("Website Password: ")
+	check_category(categories)
 	with open("temp_pass.txt", "w+") as tmp:
 		tmp.write(password)
 	
@@ -40,10 +62,17 @@ def create_pass(email, categories):
 	os.remove("temp_pass.txt")
 
 
+def get_pass(categories):
+	check_category(categories)
+	website = input("Website Name: ")
+
+	command = 'gpg --decrypt ' + website + '.enc | xclip -l 1 -selection clipboard'
+	os.system(command)
+	print("Password for " + website + " can now be pasted with Ctrl-v.\nIt will disappear after one paste.")
+
+
 
 if __name__ == "__main__":
 	print("Passman v1.0")
-	config_data = read_config_file()
-	#create_folders(config_data['categories'])
-	create_pass(config_data['email'], config_data['categories'])
+	main()
 	
